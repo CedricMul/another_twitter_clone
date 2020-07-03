2f63f77ecfd8c83a93877555923ad7910e894250
Cedric Mulvihill <cedric.mulvihill@gmail.com>
2020-07-03 13:54:39 -0400

    sneaky files

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#dr!_rcbmcn-9k_r!ocr15mzj6+lue4$@pbjyp+t43e^z2$_%w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'twitteruser',
+    'tweet',
+    'notification'
 ]
 
 MIDDLEWARE = [
--- twitteruser/urls.py
+++ twitteruser/urls.py
@@ -5,6 +5,7 @@ from twitteruser import views
 user_url_patterns = [
     path('', views.index, name='home'),
     path('profile/<int:id>', views.UserPage),
-    path('log_in/', views.UserLogin, name='log_in')
-
+    path('log_in/', views.UserLogin, name='log_in'),
+    path('new_user/', views.create_user_view),
+    path('logout/', views.logoutView)
 ]
